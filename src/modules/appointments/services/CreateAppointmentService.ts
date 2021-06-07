3419fc1b54d42b8d247fd6869abf504658844510
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2021-06-07 18:19:07 -0300

    Refatoração do projeto aplicando metodologia DDD

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,8 +1,8 @@
 import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
-import Appointment from '../models/Appointment';
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+import AppError from '@shared/errors/AppError';
 import AppointmentRepository from '../repositories/AppointmentsRepository';
-import AppError from '../errors/AppError';
 
 interface Request {
   date: Date;
