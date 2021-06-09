7784e78898311706f600241f8c831e538e9b84aa
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2021-06-09 09:00:02 -0300

    Refatoração do projeto adicionando controllers na aplicação

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,12 +1,11 @@
 import { Router } from 'express';
-import { parseISO } from 'date-fns';
-import { container } from 'tsyringe';
 
-import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
+import AppointmentsController from '@modules/appointments/infra/http/controllers/AppointmentsController';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const appointmentRouter = Router();
+const appointmentsController = new AppointmentsController();
 
 appointmentRouter.use(ensureAuthenticated);
 
@@ -16,19 +15,6 @@ appointmentRouter.use(ensureAuthenticated);
 //   return response.json(appointments);
 // });
 
-appointmentRouter.post('/', async (request, response) => {
-  const { provider_id, date } = request.body;
-
-  const parseDate = parseISO(date);
-
-  const createAppointment = container.resolve(CreateAppointmentService);
-
-  const appointment = await createAppointment.execute({
-    date: parseDate,
-    provider_id,
-  });
-
-  return response.json(appointment);
-});
+appointmentRouter.post('/', appointmentsController.create);
 
 export default appointmentRouter;
