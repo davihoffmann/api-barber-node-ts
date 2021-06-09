c4efc630bdd99559d4941756c36f31b25f461778
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2021-06-09 17:46:55 -0300

    Adicionado test criado de usuário

---
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,38 @@
+import AppError from '@shared/errors/AppError';
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import CreateUserService from '@modules/users/services/CreateUserService';
+
+describe('CreateUser', () => {
+  it('should be able to create a new user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createUserService = new CreateUserService(fakeUsersRepository);
+
+    const user = await createUserService.execute({
+      name: 'Davi Prada Hoffmann',
+      email: 'davi@email.com',
+      password: '123',
+    });
+
+    expect(user).toHaveProperty('id');
+    expect(user.email).toBe('davi@email.com');
+  });
+
+  it('should not be able to create a new user with same email from another', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createUserService = new CreateUserService(fakeUsersRepository);
+
+    await createUserService.execute({
+      name: 'Davi Prada Hoffmann',
+      email: 'davi@email.com',
+      password: '123',
+    });
+
+    expect(
+      createUserService.execute({
+        name: 'Davi Prada Hoffmann',
+        email: 'davi@email.com',
+        password: '123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
