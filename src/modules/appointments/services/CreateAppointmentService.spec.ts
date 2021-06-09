ae2af0fad572020b153331dac82615953958e22c
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2021-06-09 17:00:01 -0300

    Adicionado test para verificar a criação de 2 agendamentos na mesma data

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,5 @@
+import AppError from '@shared/errors/AppError';
+
 import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 
@@ -10,14 +12,31 @@ describe('CreateAppointment', () => {
 
     const appointment = await createAppointmentService.execute({
       date: new Date(),
-      provider_id: '1212121212',
+      provider_id: '121212',
     });
 
     expect(appointment).toHaveProperty('id');
-    expect(appointment.provider_id).toBe('1212121212');
+    expect(appointment.provider_id).toBe('121212');
   });
 
-  // it('should not be able to create two appointment on the same time', () => {
+  it('should not be able to create two appointment on the same time', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
 
-  // });
+    const appointmentDate = new Date(2020, 4, 10, 11);
+
+    await createAppointmentService.execute({
+      date: appointmentDate,
+      provider_id: '121212',
+    });
+
+    expect(
+      createAppointmentService.execute({
+        date: appointmentDate,
+        provider_id: '121212',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
