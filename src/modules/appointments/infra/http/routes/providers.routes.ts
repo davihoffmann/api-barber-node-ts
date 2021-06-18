1980c938e06f20748d3f8d4a460ef7c6bdefdffb
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2021-06-18 19:18:54 -0300

    criado controller e router para mostrar as datas e horarios dos provedores de serviço

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -2,12 +2,27 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import ProvidersControllers from '@modules/appointments/infra/http/controllers/ProvidersControllers';
+import ProviderMonthAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController';
+import ProviderDayAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderDayAvailabilityController';
 
 const providersRouter = Router();
 const providersControllers = new ProvidersControllers();
+const providerMonthAvailabilityController =
+  new ProviderMonthAvailabilityController();
+const providerDayAvailabilityController =
+  new ProviderDayAvailabilityController();
 
 providersRouter.use(ensureAuthenticated);
 
 providersRouter.get('/', providersControllers.index);
+
+providersRouter.get(
+  '/:id/month-availability',
+  providerMonthAvailabilityController.index,
+);
+providersRouter.get(
+  '/:id/day-availability',
+  providerDayAvailabilityController.index,
+);
 
 export default providersRouter;
