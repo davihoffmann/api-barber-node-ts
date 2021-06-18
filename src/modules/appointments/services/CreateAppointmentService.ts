587cb38dc02eb7efb037dcc10f9e1360db7ad85c
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2021-06-18 16:01:33 -0300

    Finalizado desenvolvimento do cadastro de agendamentos

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,4 +1,4 @@
-import { startOfHour } from 'date-fns';
+import { startOfHour, isBefore, getHours } from 'date-fns';
 import { inject, injectable } from 'tsyringe';
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
@@ -30,6 +30,20 @@ class CreateAppointmentService {
     provider_id,
   }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
+
+    if (user_id === provider_id) {
+      throw new AppError("You cant't create appointment with yourself.");
+    }
+
+    if (isBefore(appointmentDate, Date.now())) {
+      throw new AppError("You cant't create appointment on a past date.");
+    }
+
+    if (getHours(appointmentDate) < 8 || getHours(appointmentDate) > 17) {
+      throw new AppError(
+        'You can only create an appointment between 8am and 5pm',
+      );
+    }
 
     const findAppointmentInSameDate =
       await this.appointmentRepository.findByDate(appointmentDate);
