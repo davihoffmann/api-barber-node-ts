809e77d45867b98d99cc24080a1a875e9db35fe2
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2021-06-18 21:31:30 -0300

    Desenvolvido feature com a listagem dos agendamentos do prestador

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-
-import AppointmentsController from '@modules/appointments/infra/http/controllers/AppointmentsController';
-
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import AppointmentsController from '@modules/appointments/infra/http/controllers/AppointmentsController';
+import ProviderAppointmentsController from '@modules/appointments/infra/http/controllers/ProviderAppointmentsController';
 
 const appointmentRouter = Router();
 const appointmentsController = new AppointmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentRouter.use(ensureAuthenticated);
 
@@ -16,5 +16,6 @@ appointmentRouter.use(ensureAuthenticated);
 // });
 
 appointmentRouter.post('/', appointmentsController.create);
+appointmentRouter.get('/me', providerAppointmentsController.index);
 
 export default appointmentRouter;
